1658dd85a977928bed02ab38009d547996714d21
蕭立品 <a0979225002@gmail.com>
2020-01-02 00:30:07 +0800

    2020/01/02

---
+++ 練習/src/練習/test01.java
@@ -0,0 +1,32 @@
+package 練習;
+
+import javax.swing.JOptionPane;
+
+public class test01 {
+	static int a = 10;
+	test01 (int a){
+		
+	}
+	
+	static int  a(int b) {
+		int c = 10;
+		if(c==10) {
+			int ab = 10;
+		}
+		return c;
+	}
+public static void main(String[] args) {
+	test01.a(10);
+	
+//	String sb = JOptionPane.showInputDialog(567);
+//	String aString = "123";
+//	for (int i = 0; i < 2; i++) {
+//		if (sb.equals(aString )) {
+//			JOptionPane.showInputDialog(null);
+//		}else {
+//			System.out.println("你來亂的");
+//		}
+//	}
+//	System.out.println(sb);
+}
+}
